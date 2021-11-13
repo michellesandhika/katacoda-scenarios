81fe59370503c00cb0656ad9f2235d37875e75cd
swerlin <74706168+swerlin@users.noreply.github.com>
2021-11-13 12:59:31 +0800

    Create step2-foreground.sh

---
+++ sales-and-marketing/step2-foreground.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mysql -u root-p12345 -D wordpress <<EOF
+INSERT INTO wp_comments (comment_author,comment_author_email,comment_content) values("Alice","alice@example.com","Hello, nice website.");
+EOF
+echo "end of script"
