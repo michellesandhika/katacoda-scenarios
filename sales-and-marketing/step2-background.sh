91f37cb4920cf6dc78129a8181c0ab1019f42387
swerlin <74706168+swerlin@users.noreply.github.com>
2021-11-13 12:39:07 +0800

    change parameters of step2-background.sh

--- sales-and-marketing/step2-background.sh
+++ sales-and-marketing/step2-background.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-db_user='root'
-db_pw='12345'
-db_name='wordpress'
-
-mysql -u$db_user -p$db_pw -D$db_name <<EOF
+mysql -u root-p12345 -D wordpress <<EOF
 INSERT INTO wp_comments (comment_author,comment_author_email,comment_content) values("Alice","alice@example.com","Hello, nice website.");
 EOF
