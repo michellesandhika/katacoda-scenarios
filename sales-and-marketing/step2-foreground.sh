ae306369e3da96d730b53e260df544876781ff98
swerlin <74706168+swerlin@users.noreply.github.com>
2021-11-13 14:40:06 +0800

    Update step2-foreground.sh

--- sales-and-marketing/step2-foreground.sh
+++ sales-and-marketing/step2-foreground.sh
@@ -2,8 +2,7 @@
 
 docker exec -it mysql bash
 
-mysql -u root-p12345 << EOF
-use wordpress;
-SELECT comment_content FROM wp_comments;
-INSERT INTO wp_comments (comment_author,comment_author_email,comment_content) values("Alice","alice@example.com","Hello, nice website.");
+mysql -u root -p12345 << EOF
+  use wordpress;
+  INSERT INTO wp_comments (comment_author,comment_author_email,comment_content) values("Alice","alice@example.com","Hello, nice website.");
 EOF
