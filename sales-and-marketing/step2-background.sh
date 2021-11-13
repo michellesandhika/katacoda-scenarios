a198d775c5a30e8aba9c9797692affa5033d1278
swerlin <74706168+swerlin@users.noreply.github.com>
2021-11-13 14:08:14 +0800

    Update step2-background.sh

--- sales-and-marketing/step2-background.sh
+++ sales-and-marketing/step2-background.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
-mysql -u root -p12345 <<EOF
+mysql -u root -p12345 << EOF
   use wordpress;
   INSERT INTO wp_comments (comment_author,comment_author_email,comment_content) values("Alice","alice@example.com","Hello, nice website.");
 EOF
