90d37714bbe3d5b4edf1e636cbd6d0ee11c9a668
swerlin <74706168+swerlin@users.noreply.github.com>
2021-11-13 12:28:47 +0800

    update step2-background.sh

--- sales-and-marketing/step2-background.sh
+++ sales-and-marketing/step2-background.sh
@@ -2,8 +2,8 @@
 
 db_user='root'
 db_pw='12345'
+db_name='wordpress'
 
-mysql -u$db_user -p$db_pw <<EOF
-use wordpress;
+mysql -u$db_user -p$db_pw -D$db_name <<EOF
 INSERT INTO wp_comments (comment_author,comment_author_email,comment_content) values("Alice","alice@example.com","Hello, nice website.");
 EOF
